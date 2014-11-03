2dd33bf202a41124bfe462e6499ef87db9dee8fd
ksoichiro <soichiro.kashima@gmail.com>
2014-11-03 22:56:03 +0900

    Bumped version to 0.1.2.

--- adbs.rb
+++ adbs.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class Adbs < Formula
   homepage "https://github.com/ksoichiro/adbs"
-  version "0.1.1"
+  version "0.1.2"
 
   if Hardware.is_64_bit?
-    url "https://github.com/ksoichiro/adbs/releases/download/v0.1.1/adbs-0.1.1-bin-darwin-amd64.zip"
-    sha1 "d26ec386ff5645f25fece0d4ff041ae8a35c50b1"
+    url "https://github.com/ksoichiro/adbs/releases/download/v0.1.2/adbs-0.1.2-bin-darwin-amd64.zip"
+    sha1 "97e72d4f133439ede33caa4802283d789f4082a2"
   else
-    url "https://github.com/ksoichiro/adbs/releases/download/v0.1.1/adbs-0.1.1-bin-darwin-386.zip"
-    sha1 "521e99fc747675a848f3924013a4b406604ddbde"
+    url "https://github.com/ksoichiro/adbs/releases/download/v0.1.2/adbs-0.1.2-bin-darwin-386.zip"
+    sha1 "cfdc4b1ae94752870054a637e3b75687b7c704c3"
   end
 
   def install
