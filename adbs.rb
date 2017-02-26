580e58455c3bfd8bee56a1495dd6d8cfccbe2c12
Soichiro Kashima <soichiro.kashima@gmail.com>
2017-02-26 18:22:56 +0900

    Fix warnings

--- adbs.rb
+++ adbs.rb
@@ -4,12 +4,12 @@ class Adbs < Formula
   homepage "https://github.com/ksoichiro/adbs"
   version "0.1.2"
 
-  if Hardware.is_64_bit?
+  if Hardware::CPU.is_64_bit?
     url "https://github.com/ksoichiro/adbs/releases/download/v0.1.2/adbs-0.1.2-bin-darwin-amd64.zip"
-    sha1 "97e72d4f133439ede33caa4802283d789f4082a2"
+    sha256 "04a3f03635473966d75038903d001915b9c77bb7c8599bc0f03456b6dc08117e"
   else
     url "https://github.com/ksoichiro/adbs/releases/download/v0.1.2/adbs-0.1.2-bin-darwin-386.zip"
-    sha1 "cfdc4b1ae94752870054a637e3b75687b7c704c3"
+    sha256 "a7d514c4c6c59022d36ccc9f157c18bb2d43caa2a08063a137a9c8ff628b32b3"
   end
 
   def install
